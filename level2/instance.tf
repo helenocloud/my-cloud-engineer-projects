0f31dba448870f59763891147866a1e64e2e5c33
Helen <helenofalodun@gmail.com>
2022-09-13 15:44:42 -0400

    Add load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -22,8 +22,6 @@ resource "aws_instance" "public" {
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public[0].id
 
-  user_data = file("user-data.sh")
-
   tags = {
     Name = "${var.env_code}-public"
   }
@@ -39,7 +37,7 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["72.83.207.196/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -67,7 +65,9 @@ resource "aws_instance" "private" {
   instance_type          = "t2.micro"
   key_name               = "main"
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = aws_subnet.private[1].id
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
+
+  user_data = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-private"
@@ -77,14 +77,22 @@ resource "aws_instance" "private" {
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
     description = "SSH from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+ }
+
+   ingress {
+    description = "HTTP from load balancer"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
   }
 
   egress {
