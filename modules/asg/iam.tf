675832862b453241ef3768d2ff680b474a45a25b
Helen <helenofalodun@gmail.com>
2022-09-18 22:40:49 -0400

    Add sg & iam

---
+++ modules/asg/iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "main" {
+  name        = var.env_code
+  description = "EC2 IAM role for SSM access"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : ["ec2.amazonaws.com"]
+          },
+          "Effect" : "Allow",
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_instance_profile" "main" {
+  role = aws_iam_role.main.name
+  name = var.env_code
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
